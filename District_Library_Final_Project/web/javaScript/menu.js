4fab6c8a1fbe561ed46f42e73c9ae262200e3924
lauramarsantana <lmsantanas@udistrital.edu.co>
2025-02-03 10:02:31 -0500

    Añadiendo login

---
+++ District_Library_Final_Project/web/javaScript/menu.js
@@ -0,0 +1,23 @@
+const menusItemsDropDown = document.querySelectorAll('.menu-item-dropdown');
+const sidebar = document.getElementById('sidebar');
+const menuBtn = document.getElementById('menu-btn');
+
+menuBtn.addEventListener('click',()=>{
+	sidebar.classList.toggle('minimize');
+});
+
+menusItemsDropDown.forEach((menuItem)=>{
+	menuItem.addEventListener('click',()=>{
+		const subMenu = menuItem.querySelector('.sub-menu'); /*solo vamos a obterne un solo elemento de nuestro menuItem*/
+		const isActive = menuItem.classList.toggle('sub-menu-toggle'); /*para saber si está abierto o cerrado*/
+		if(subMenu){/*verifica si el elemento existe*/
+			if(isActive){
+				subMenu.style.height = `${subMenu.scrollHeight + 6}px`;
+				subMenu.style.padding = '0.2rem 0';
+			}else{
+				subMenu.style.height = '0';
+				subMenu.style.padding = '0';
+			}
+		}
+	});
+});
